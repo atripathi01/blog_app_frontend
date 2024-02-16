ee2f99fda1a535b46acf87e340748f5f2f49d3c9
Ayush Tripathi <ayushtripathi@Ayushs-Laptop.local>
2024-02-16 15:51:17 +0530

    change : fix build issue

--- src/app/blogs/layout.tsx
+++ src/app/blogs/layout.tsx
@@ -1,10 +1,12 @@
 "use client"
+import { useContext, useEffect } from 'react';
 // import type { Metadata } from "next";
 // import { Inter } from "next/font/google";
 import Footer from '../../../components/footer/footer';
 import Navbar from '../../../components/navbar/navbar';
-import { AuthContextProvider } from '../../../context/AuthContext';
+import { AuthContext, AuthContextProvider } from '../../../context/AuthContext';
 import '../globals.css';
+import { useRouter } from 'next/navigation';
 
 
 export default function AuthLayout({
