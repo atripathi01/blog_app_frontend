f98920b19614f050cac063f1539d0263d5adc735
Ayush Tripathi <ayushtripathi@Ayushs-Laptop.local>
2024-02-17 22:14:27 +0530

    change: change direction rule

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,7 @@ const nextConfig = {
         return [
           {
             source: "/",
-            destination: "/blogs",
+            destination: "/auth/login",
             permanent: true,
           },]}
 };
